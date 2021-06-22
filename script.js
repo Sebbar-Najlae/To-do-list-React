93cc8b1136e8ecb7cd64f46965f80f03724b774a
sebbar najlae <sebbar.najlae.dev@gmail.com>
2021-06-22 13:11:30 +0100

    Final

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ class Task extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   render() {
     let class_name = 'task'
     class_name += this.props.done ? ' task-success' : ' task-info';
-  
+
     return (
       <div className={class_name}>
         <span>{this.props.value}</span>
@@ -21,22 +21,22 @@ class Task extends React.Component {
 class App extends React.Component {
 
   tasksArray = [
-      {value: 'Create branch', done: true},
-      {value: 'Version-0 of project', done: false},
-      {value: 'Prototype', done: false}
-    ];
+    { value: 'Create branch', done: true },
+    { value: 'Version-0 of project', done: false },
+    { value: 'Prototype', done: false }
+  ];
 
   constructor(props) {
     super(props)
 
-    
+
   }
 
   render() {
-   
+
     let tasksArrayMap = this.tasksArray.map((task, i) => {
       return (
-        <Task 
+        <Task
           key={i}
           value={task.value}
           done={task.done}
@@ -47,13 +47,13 @@ class App extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-sm-offset-3 text-center">
+          <div className="col-sm-6 col-sm-offset-3 text-center ">
             <h1>To-do List</h1>
             <form
               id="form-add"
               className="form-horizontal">
               <div className="input-group">
-                <input type="text" id="addInput" className="form-control"  placeholder="Add task..." />
+                <input type="text" id="addInput" className="form-control" placeholder="Add task..." />
                 <div className="input-group-btn">
                   <button type="submit" className="btn btn-default">
                     <span className="glyphicon glyphicon-plus-sign"></span>
@@ -63,7 +63,7 @@ class App extends React.Component {
             </form>
 
             {tasksArrayMap}
-            
+
           </div>
         </div>
       </div>
